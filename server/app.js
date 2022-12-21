35791726f29dc890ed49b7239eb9bd3d98ff7db3
Hemant Grid <hemantgrid@192.168.1.4>
2022-12-21 22:28:26 +0530

    lesson 4 :  resolve function and graphiql ui

--- server/app.js
+++ server/app.js
@@ -1,11 +1,13 @@
 const express = require('express');
-const graphqlHTTP = require('express-graphql');
+const { graphqlHTTP } = require('express-graphql'); //destructuring error
+const schema = require('./schema/schema');
 
 const app = express();
 
-// app.use('/graphql', graphqlHTTP({
-
-// }));
+app.use('/graphql', graphqlHTTP({
+    schema,
+    graphiql:true //graphiql ui 
+}));
 
 const PORT = 4000;
 app.listen(PORT,() => {
--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,6 +1,14 @@
 const graphql = require('graphql');
+const _=require('lodash');
 
 const { GraphQLObjectType, GraphQLString, GraphQLSchema } = graphql;
+
+// dummy data 
+let books = [
+    {name:'Book 1', genre: 'Fantasy', id: '1'},
+    {name:'Book 2', genre: 'Fantasy', id: '2'},
+    {name:'Book 3', genre: 'Sci-Fi', id: '3'},
+];
 
 const BookType = new GraphQLObjectType({
     name: 'Book',
@@ -19,11 +27,12 @@ const RootQuery = new GraphQLObjectType({
             args: {id: {type: GraphQLString}},
             resolve(parent, args){
                 // code to ger data from db / other source
+                return _.find(books,{id: args.id});
             }
         }
     }
 });
 
-module.export = new graphql.GraphQLSchema({
+module.exports = new GraphQLSchema({ //module.exports error
     query: RootQuery,
 })
